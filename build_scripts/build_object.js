a93392b99022e3ee272fc9c9222ac7b5531927d7
Austin Meyers <meyersaustin22@gmail.com>
2016-01-25 12:10:25 -0800

    Moved from python to just node ES6

---
+++ build_scripts/build_object.js
@@ -0,0 +1,13 @@
+const https = require('https');
+
+https.get('https://www.riddles.com/201', (res) => {
+  console.log('statusCode: ', res.statusCode);
+  console.log('headers: ', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
