edac71d308eb9390d6487ac1da68c83de8a7c114
Austin Meyers <meyersaustin22@gmail.com>
2016-01-26 08:55:03 -0800

    Update build script modularity

--- build_scripts/build_object.js
+++ build_scripts/build_object.js
@@ -1,13 +1,26 @@
 const https = require('https');
 
-https.get('https://www.riddles.com/201', (res) => {
-  console.log('statusCode: ', res.statusCode);
-  console.log('headers: ', res.headers);
+//Function that makes requests to the parameterized url
+function makeRequest(url) {
+	//Make the request, logging the status code and headers
+	https.get(url, (res) => {
+		console.log('statusCode: ', res.statusCode);
+		console.log('headers: ', res.headers);
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
+		//Chunk the data and write it out
+		res.on('data', (d) => {
+			process.stdout.write(d);
+		});
 
-}).on('error', (e) => {
-  console.error(e);
-});
+		//Handle the end of the response
+		res.on('end', () => {
+			console.log('\nResponse End')
+		});
+
+	//Handle errors
+	}).on('error', (e) => {
+		console.error(e);
+	});
+};
+
+makeRequest('https://www.riddles.com/201');
