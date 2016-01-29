4719c560f40963afb4e5a2cd7cb4e51a85791537
Austin Meyers <meyersaustin22@gmail.com>
2016-01-29 12:50:56 -0800

    Push objects into array

--- build_scripts/build_object.js
+++ build_scripts/build_object.js
@@ -1,30 +1,38 @@
 //Require jsdom library
 const jsdom = require("jsdom");
+var data = [];
 
 //Function that makes requests to the parameterized url
 function makeRequest(urls) {
+	var jsonObj;
 	//Set jsdom environment
 	jsdom.env({
 		url: urls,
 			//Parse html with jquery
 	  		scripts: ["http://code.jquery.com/jquery.js"],
 	  		done: (err, window) => {
-	  			if (err == null) {
+	  			//Check for error
+	  			if (err) {
+	  				console.log("Error: " + err);
+	  			} else {
 	  				var title = window.$('h1.panel-title[itemprop="name"]').first().text();
   					var body = window.$('.panel-body div[itemprop="text"] p');
 		  			var question = body[0].innerHTML;
 		  			var answer = body[1].innerHTML;
-		  			var jsonObj = {
+		  			//Build riddle object
+		  			jsonObj = {
 		  				title: title,
 		  				quesiton: question,
 		  				answer: answer
 		  			};
-		    		console.log(jsonObj);
-	  			} else {
-	  				console.log("Error: " + err);
+		  			data.push(jsonObj);
+		  			console.log(data);
 	  			}
 	  		}
 	});
 };
 
-makeRequest('https://www.riddles.com/2');
+
+for (var i = 2; i <= 5; i++) {
+	makeRequest("https://www.riddles.com/" + i.toString());
+};
