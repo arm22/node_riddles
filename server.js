c938fc22d837ed055fe44c36d1a401868da3f071
Austin Meyers <meyersaustin22@gmail.com>
2016-02-07 20:42:36 -0800

    Respond correctly only to GET requests

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+//Need http
 var http = require("http");
-var server = http.createServer((request, response) => {
-	response.writeHead(200, {"Content-Type": "application/json"});
-	var data = JSON.stringify({ message: "Hello world"});
-	response.end(data);
+//DB client
+const MongoClient = require('mongodb').MongoClient;
+// Connection URL 
+var url = 'mongodb://localhost:27017/node_riddles';
+
+var server = http.createServer((req, res) => {
+	if (req.method === 'GET') {
+		res.writeHead(200, {"Content-Type": "application/json"});
+		var data = JSON.stringify({ message: "Hello world"});
+		res.end(data);
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}	
 });
 
 server.listen(8080);
