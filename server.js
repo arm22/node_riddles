460bb69b052970d639542fc156e75926b51eaf02
Austin Meyers <meyersaustin22@gmail.com>
2016-02-09 09:40:01 -0800

    Get all documents and log them

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ const MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/node_riddles';
 
 var server = http.createServer((req, res) => {
-	if (req.method === 'GET') {
+	if (req.method === 'GET' && req.url === '/favicon.ico') {
+		res.statusCode = 404;
+		res.end();
+	} else if (req.method === 'GET') {
 		res.writeHead(200, {"Content-Type": "application/json"});
 		MongoClient.connect(url, (err, db) => {
 			if (err) {
-				console.log(err);
+				console.log("Mongo Error: " + err);
 			} else {
 				console.log("Connected correctly to server");
 				var collection = db.collection('riddles');
-				var result = collection.find().toArray();
-				console.log(result);
-				db.close();
-				res.end(JSON.stringify(result));
+				var cursor = collection.find({}).toArray((err, docs) => {
+					if (err) {
+						console.log("Mongo Error: " + err);
+					} else {
+						console.log(docs);
+						db.close();
+					}
+				});
 			}
+			res.end();
 		});
 	} else {
 		res.statusCode = 404;
