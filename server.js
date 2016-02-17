3a90193fbef6d0f722504024aa468e3c8f1307d0
Austin Meyers <meyersaustin22@gmail.com>
2016-02-16 16:38:50 -0800

    Get integration working

--- server.js
+++ server.js
@@ -1,82 +1,54 @@
-//Need http
+//Require Express & body-parser
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-var bodyParser = require('body-parser');
+//Set the app to use the body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //DB client
 const MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/node_riddles';
+const url = 'mongodb://localhost:27017/node_riddles';
 
 app.get('/random', (req, res) => {
 	res.writeHead(200, {"Content-Type": "application/json"});
-		//Create connection to server
-		MongoClient.connect(url, (err, db) => {
-			if (err) {
-				console.log("Mongo Error: " + err);
-			} else {
-				console.log("Connected correctly to DB");
-				var collection = db.collection('riddles');
-				//Query for a random riddle from mongo
-				collection.aggregate([{ $sample: { size: 1 }}], (err, docs) => {
-					if (err) {
-						console.log("Mongo Error: " + err);
-					} else {
-						//end response with random riddle in JSON
-						db.close();
-						res.end(JSON.stringify(docs[0]))
-					}
-				});
-			}
-		});
+	var data = getRandom((data) => {
+		res.end(JSON.stringify(data));
+	});
 });
 
 app.post('/random', (req, res) => {
 	//need slack token
-	console.log(req.body.token);
 	if (req.body.token === key) {
-		//Create connection to server
-		MongoClient.connect(url, (err, db) => {
-			if (err) {
-				console.log("Mongo Error: " + err);
-				res.writeHead(404);
-			} else {
-				console.log("Connected correctly to DB");
-				var collection = db.collection('riddles');
-				//Query for a random riddle from mongo
-				collection.aggregate([{ $sample: { size: 1 }}], (err, docs) => {
-					if (err) {
-						console.log("Mongo Error: " + err);
-						res.writeHead(404);
-					} else {
-						//end response with random riddle in JSON
-						db.close();
-						riddle = docs[0];
-						json_resp = {
-							"response_type": "in_channel",
-							"text": riddle.question
-						}
-						res.writeHead(200, {"Content-Type": "application/json"});
-						res.end(JSON.stringify(json_resp));
-					}
-				});
-			}
+		var data = getRandom((data) => {
+			var question = data.question.replace(/\r?\n|\r/g, " ");
+			res.end(JSON.stringify(question));
 		});
-		// {
-	 //    "response_type": "in_channel",
-	 //    "text": "It's 80 degrees right now.",
-	 //    "attachments": [
-	 //        {
-	 //            "text":"Partly cloudy today and tomorrow"
-	 //        }
-	 //    ]
-		// }
-	} else {
-		res.writeHead(404);
 	}
 });
 
+//common function to get a random riddle
+function getRandom(callback) {
+	//Create connection to server
+	MongoClient.connect(url, (err, db) => {
+		if (err) {
+			throw err;
+		} else {
+			var collection = db.collection('riddles');
+			//Query for a random riddle from mongo
+			collection.aggregate([{ $sample: { size: 1 }}], (err, docs) => {
+				if (err) {
+					throw err;
+				} else {
+					//return the riddle data
+					db.close();
+					callback(docs[0]);
+				}
+			});
+		}
+	});
+}
+
 //start server
 app.listen(8080,() => {
-  console.log('Example app listening on port 8080!');
+  console.log('Node Riddles listening on port 8080!');
 });
