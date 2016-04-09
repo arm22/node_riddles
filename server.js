f474340b67f5d9a42914712e03956fbeba69f226
Austin Meyers <meyersaustin22@gmail.com>
2016-04-08 20:38:14 -0700

    Revert server changes

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const fs = require('fs');
 const https = require('https');
+const request = require('request');
 const kue = require('kue');
+const q = kue.createQueue({
+  prefix: 'q',
+  redis: {
+    port: 6379,
+    host: '127.0.0.1',
+    db: 1
+  }
+});
+kue.app.listen(3000);
+
+q.process('post-resp', function(job, done){
+  send(job, done);
+});
+
+function send(job, done) {
+  request({
+    uri: job.data.uri,
+    method: job.data.method,
+    json: job.data.json
+  });
+  done();
+}
+
 //Require Express & body-parser
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -12,58 +36,36 @@ app.use(express.static('public'));
 //Use nconf for keystore
 const nconf = require('nconf');
 nconf.file({file: './keys.json'});
-var db;
-
-function setupDb(callback)
-{
-  db = require("./mongo.js");
-  db.init(nconf, callback);
-}
-
-setupDb(function() {
-  console.log("connected");
-});
-
-//Connect to redis
-const q = kue.createQueue({
-  prefix: 'q',
-  redis: {
-    host: nconf.get('redis:host'),
-    port: nconf.get('redis:port'),
-    db: nconf.get('redis:db')
-  }
-});
-
-q.process('post-resp', function(job, done){
-  send(job, done);
-});
 
 var options = {
-  ca: fs.readFileSync('ssl/www_slack-riddle_xyz.ca-bundle'),
-  key: fs.readFileSync('ssl/private-key.key'),
-  cert: fs.readFileSync('ssl/www_slack-riddle_xyz.crt')
+  ca: fs.readFileSync('../ssl/www_slack-riddle_xyz.ca-bundle'),
+  key: fs.readFileSync('../ssl/private-key.key'),
+  cert: fs.readFileSync('../ssl/www_slack-riddle_xyz.crt')
 }
 //Start server
-https.createServer(options, app).listen(8080);
+https.createServer(options, app).listen(443);
+
+//DB client
+const MongoClient = require('mongodb').MongoClient;
 
 app.get('/random', (req, res) => {
-	var data = getRandom((data) => {
-		//Replace any whitespace
-		var question = data.question.replace(/\r?\n|\r/g, " ");
-		var answer = data.answer.replace(/\r?\n|\r/g, " ");
-		//Build JSON riddle
-		var message = {
-			"question": question,
-			"answer": answer
-		};
-		//Set the header and end the response stream
-		res.writeHead(200, {"Content-Type": "application/json"});
-		res.end(JSON.stringify(message));
-	});
+  var data = getRandom((data) => {
+    //Replace any whitespace
+    var question = data.question.replace(/\r?\n|\r/g, " ");
+    var answer = data.answer.replace(/\r?\n|\r/g, " ");
+    //Build JSON riddle
+    var message = {
+      "question": question,
+      "answer": answer
+    };
+    //Set the header and end the response stream
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(message));
+  });
 });
 
 app.post('/random', (req, res) => {
-	if (req.body.token === nconf.get('token')) {
+  if (req.body.token === nconf.get('token')) {
     var text = req.body.text;
     //Respond to help command
     if (text.toLowerCase() === "help") {
@@ -77,20 +79,20 @@ app.post('/random', (req, res) => {
     } else {
       //Convert param to int
       var wait = parseInt(req.body.text);
-  		var data = getRandom((data) => {
-  			//Replace any whitespace
-  			var question = data.question.replace(/\r?\n|\r/g, " ");
-  			var answer = data.answer.replace(/\r?\n|\r/g, " ");
-  			//Build slack riddle
-  			var message = {
-  								    "response_type": "in_channel",
+      var data = getRandom((data) => {
+        //Replace any whitespace
+        var question = data.question.replace(/\r?\n|\r/g, " ");
+        var answer = data.answer.replace(/\r?\n|\r/g, " ");
+        //Build slack riddle
+        var message = {
+                      "response_type": "in_channel",
                       "text": "Riddle:",
                       "attachments": [
                         {
                           "text": question
                         }
                       ]
-  								};
+                  };
         
         if (!isNaN(wait)) {
           var send_time = new Date();
@@ -101,12 +103,7 @@ app.post('/random', (req, res) => {
             method: "POST",
             json: {
               "response_type": "in_channel",
-              "text": "Answer:",
-              "attachments": [
-                        {
-                          "text": answer
-                        }
-                      ]
+              "text": answer
             }
           }).delay(send_time)
           .save();
@@ -114,14 +111,32 @@ app.post('/random', (req, res) => {
           //If wait is not a number
 
         }
-  			//Set the header and end the response stream
-  			res.writeHead(200, {"Content-Type": "application/json"});
-  			res.end(JSON.stringify(message));
-  		});
+        //Set the header and end the response stream
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(message));
+      });
     }
-	}
+  }
 });
 
-
-
-
+//common function to get a random riddle
+function getRandom(callback) {
+  //Create connection to server
+  MongoClient.connect("mongodb://" + nconf.get('mongodb:host') + ":" + nconf.get('mongodb:port') + "/" + nconf.get('mongodb:collection'), (err, db) => {
+    if (err) {
+      throw err;
+    } else {
+      var collection = db.collection('riddles');
+      //Query for a random riddle from mongo
+      collection.aggregate([{ $sample: { size: 1 }}], (err, docs) => {
+        if (err) {
+          throw err;
+        } else {
+          //return the riddle data
+          db.close();
+          callback(docs[0]);
+        }
+      });
+    }
+  });
+};
